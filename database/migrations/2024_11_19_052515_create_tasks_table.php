dabfe499396b6f5f2799287fa1095ebf2315cb06
DininduKavinda <kavinda373@gmail.com>
2024-11-19 11:08:21 +0530

    laravel app creating

---
+++ database/migrations/2024_11_19_052515_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->unsignedInteger('user_id');
+            $table->increments('id');
+            $table->string('title',50);
+            $table->text('description')->nullable();
+            $table->date('due_date');
+            $table->enum('priority', ['High' , 'Medium','Low']);
+            $table->boolean('is_complete')->default(false);
+            $table->boolean('is_paid')->default(false);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
